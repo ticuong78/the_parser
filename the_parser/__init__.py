4542005f513084cb06a5632c3717d413ad0b702d
ticuong78 <cuongdayne17@gmail.com>
2025-01-17 16:58:16 +0700

    Replace _env.py and _ext.py with env.py and ext.py respectively

--- the_parser/__init__.py
+++ the_parser/__init__.py
@@ -1,8 +1,8 @@
 from the_parser.parser._EnvParser import EnvParser
 from the_parser.preprocess._ConfigPreProcess import ConfigPreProcessor
 
-from _env import AVAILABLE_ENVS
-from _ext import AVAILABLE_EXTS
+from env import AVAILABLE_ENVS
+from ext import AVAILABLE_EXTS
 
 preprocessor = ConfigPreProcessor()
 envparser = EnvParser(preprocessor)
